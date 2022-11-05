7b9d9c7573dabc8bf0e2734e7ba870c084723008
Zaw Lay Pyay Thar <zawlaytest3@gmail.com>
2022-11-05 18:46:42 +0630

    first commit

---
+++ zlpt/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <Header></Header>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+  }
+};
+</script>
---
+++ zlpt/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-app-bar
+    elevation="4"
+    dense
+    
+    >
+    <router-link to="/" class="mx-2 navlink">Home</router-link>
+    <!-- <v-spacer></v-spacer> -->
+    <router-link to="/about" class="mx-2 navlink">About</router-link>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+ name: "header",
+
+}
+</script>
+
+<style scoped>
+
+.navlink {
+  text-decoration: none;
+}
+.navlink:hover {
+    text-decoration: underline;
+}
+
+</style>
