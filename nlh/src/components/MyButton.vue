de3e78be4e1122d8316955d46582d03a9fabb510
Nlhmmh <nlh2012.8@gmail.com>
2022-11-05 10:36:36 +0630

    nlh - First Commit

---
+++ nlh/src/components/MyButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button class="my-btn">Click Me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyButton",
+};
+</script>
+
+<style>
+.my-btn {
+  padding: 20px;
+  color: blue;
+}
+
+.my-btn :hover {
+  color: red;
+  text-decoration: underline;
+}
+</style>
