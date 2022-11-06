e8db1329c15676a6a857486d08aef577434fd1e4
ChueKhet <khinchanmyaethu.kcmt2000@gmail.com>
2022-11-06 07:49:59 +0630

    chuekhet - First commit

---
+++ ChueKhet/src/components/MyButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <button> Click me</button>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+my-button{
+        padding: 30px;
+        color :blueviolet;
+       
+      }
+
+      button:hover{
+        text-decoration: underline;
+
+      }
+</style>
---
+++ ChueKhet/src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <v-container>
+            <v-card class = "pa-5 pt-6" elevation="4" max-width="300">
+        <v-btn elevation="2">See More</v-btn>
+        <v-img max-height="120"
+        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwallpaperaccess.com%2Fnew&psig=AOvVaw2V2p8FyPRiqAV-7WPffaaM&ust=1667750169943000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCLjWxtWzl_sCFQAAAAAdAAAAABAE"
+        ></v-img>
+        <v-card-title>About This Page</v-card-title>
+        <v-card-text>Our Shop supports various features..</v-card-text>
+        <v-btn disabled>Disable</v-btn>
+            </v-card>
+    </v-container>
+    </div>
+</template>
+<script>
+export default {
+    name:about,
+}
+</script>
+<style>
+
+</style>
